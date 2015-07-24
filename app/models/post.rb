60465fdae351920a442e8af1c6ef017d8ef63cd4
kidusyohans <kidusyohans@hotmail.com>
2015-07-24 05:00:37 +0000

    Assignment 38 completed

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,10 @@ class Post < ActiveRecord::Base
     belongs_to :user
     
     default_scope { order('created_at DESC') }
+    scope :ordered_by_title, -> { update(ordered_by_title: true) }
+    scope :ordered_by_reverse_created_at, -> { 
+    	update(ordered_by_reverse_created_at: true)
+  }
+
+
 end
