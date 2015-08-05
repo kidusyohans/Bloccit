a84932fca138aff843ba91645698c6ebe8b94ba0
kidusyohans <kidusyohans@hotmail.com>
2015-08-05 18:37:36 +0000

    Checkpoint 42 completed, Validating-Posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -10,6 +10,11 @@ class Post < ActiveRecord::Base
     scope :ordered_by_reverse_created_at, -> { 
     	update(ordered_by_reverse_created_at: true)
   }
+  
+  validates :title, length: { minimum: 5 }, presence: true
+  validates :body, length: { minimum: 20 }, presence: true
+  validates :topic, presence: true
+  validates :user, presence: true
 
 
 end
