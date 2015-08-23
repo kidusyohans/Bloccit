ca85bdde74df89bc1d755702bc253f83ca0092fa
kidusyohans <kidusyohans@hotmail.com>
2015-08-23 19:46:29 +0000

    assignment-48-RSpec completed

--- spec/vote_spec.rb
+++ spec/vote_spec.rb
@@ -1,11 +1,19 @@
 describe Vote do
+	def valid?
+		(@vote == 1) || (@vote == -1)
+	end
   describe "validations" do
+  	before do
+  		let(:good_v) { Vote.new(value: 1) }
+  		let(:bad_v) { Vote.new(value: -1) }
+  		let(:no_v) { Vote.new(value:  2) }
+  	end
     describe "value validation" do
       it "only allows -1 or 1 as values" do
-        # your expectations here
-         expect(good_v.valid?).to eq(true) 
-         expext(bad_v.valid?).to eq(true)
-         expect(no_v.valid?).to eq(false)
+        
+         expect(good_vote.valid?).to eq(true) 
+         expext(bad_vote.valid?).to eq(true)
+         expect(no_vote.valid?).to eq(false)
       end
     end
   end
