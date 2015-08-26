e8fe05de346e830e12b10239f0542537859c78ab
kidusyohans <kidusyohans@hotmail.com>
2015-08-26 04:29:08 +0000

    assignment-51-Favoriting

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,14 +1,27 @@
 require 'rails_helper'
 
 describe User do
-	
-	#include TestFactories
-	
-	describe "#favorited(post)" do
-		xit "returns `nil` if the user has not favorited the post" do
-		end
-		
-		xit "returns the appropriate favorite if it exists" do
-		end
-	end
-end
+
+ include TestFactories
+
+ describe "#favorited(post)" do
+
+   before do
+     @post = associated_post
+     @user = authenticated_user
+   end
+
+   it "returns `nil` if the user has not favorited the post" do
+     expect( @user.favorited(@post) ).to be_nil
+   end
+
+   it "returns the appropriate favorite if it exists" do
+     favorite = Favorite.new
+     expect( @user.favorite ).to eq( favorited(@post) )
+   end
+
+   it "returns `nil` if the user has favorited another post" do
+
+   end
+  end
+end 
